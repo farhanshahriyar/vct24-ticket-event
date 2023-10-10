691cff8a4178026f92b75462d6fc1fbd719cf86e
farhanshahriyar <farhan.abir9999@gmail.com>
2023-10-10 20:06:14 +0600

    added some functionalities for register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,18 +3,24 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import auth from '../../firebase/firebase.config';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
+import { BiShowAlt } from 'react-icons/bi';
+import { useState } from 'react';
 
 const Register = () => {
     // function navigate korbe from the hook theke pawar jonne
     const navigate = useNavigate();
-
-
+    const [showPassword, setShowPassword] = useState(false);
+    const [registerError, setRegisterError] = useState(''); // error message show korar jonne
+    const [success, setSuccess] = useState(''); // success message show korar jonne
 
     const handleRegister = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
+
+        // reset error and success message
+        setRegisterError('');
 
         if (password.length < 6) {
             toast.error('Password must be at least 6 characters long!');
@@ -38,8 +44,9 @@ const Register = () => {
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
-            console.log(user);
+            // console.log(user);
             localStorage.setItem('user', JSON.stringify(user)); // Store user in local storage
+            setSuccess('User registered successfully!');
             toast.success('User registered successfully!');
             
             // Navigate korbe root directory after successful registration houar por
@@ -47,8 +54,9 @@ const Register = () => {
         })
         .catch((error) => {
             // Handle errors jodi khay 
-            console.error("Error registering the user:", error.message);
+            // console.error("Error registering the user:", error.message);
             toast.error(error.message);
+            setRegisterError(error.message);
         });
     }
 
@@ -76,18 +84,29 @@ const Register = () => {
                 <div className="flex items-center justify-between">
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
                 <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    <span onClick={() => setShowPassword(!showPassword)}
+                     href="#" className="text-xl text-indigo-600 hover:text-indigo-500"><BiShowAlt/></span>
                 </div>
                 </div>
                 <div className="mt-2">
-                <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <input 
+                 id="password" 
+                 name="password" 
+                 type={ showPassword ? "text" : "password" }
+                 autoComplete="current-password" 
+                 required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 font-sara"></input>
                 </div>
+                
             </div>
 
             <div>
                 <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
             </div>
             </form>
+            {
+                registerError && <p className="mt-4 text-center text-sm text-red-600">{registerError}</p>
+              
+            }
 
             <p className="mt-10 text-center text-sm text-gray-500">
             Already member?
